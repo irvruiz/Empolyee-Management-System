dbf8baa4a9e871629035fb93701ffa3f94966556
Irvin <irvinruiz44@yahoo.com>
2022-02-17 18:18:39 -0700

    added js

--- index.js
+++ index.js
@@ -1,33 +1,86 @@
 const { prompt } = require("inquirer");
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
-require('dotenv').config()
+require("dotenv").config();
 
 const db = mysql.createConnection(
-    {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    },
-    console.log("Connected to the company_db database....")
+  {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  console.log("Connected to the company_db database....")
 );
 
 db.connect((err) => {
-    if (err) {
-        throw error;
-    }
+  if (err) {
+    throw error;
+  }
 });
 //inqurirer
-promptUser()
+promptUser();
 function promptUser() {
-    return inquirer.prompt([
-        {
-            type: "list",
-            name: "displayChoices",
-            message: "Please choose an option",
-            choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role", "delete an employee", "delete a department",  "delete a role", "view a deparment budget"]
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "displayChoices",
+        message: "Please choose an option",
+        choices: [
+          "view all departments",
+          "view all roles",
+          "view all employees",
+          "add a department",
+          "add a role",
+          "add an employee",
+          "update an employee role",
+          "delete an employee",
+          "delete a department",
+          "delete a role",
+          "view a deparment budget",
+        ],
+      },
+    ])
+    .then((selectedOption) => {
+      switch (selectedOption.displayChoices) {
+        case "view all departments":
+          viewDepartments();
+          break;
+        case "view all roles":
+          viewRoles();
+          break;
+        case "view all employees":
+          viewEmployees();
+          break;
+        case "add a department":
+          addDepartment();
+          break;
+        case "add a role":
+          addRole();
+          break;
+        case "add an employee":
+          addEmployee();
+          break;
+        case "update an employee role":
+          updateEmployeeRole();
+          break;
+        case "delete an employee":
+          deleteEmployee();
+          break;
+        case "delete a department":
+          deleteDepartment();
+          break;
+        case "delete a role":
+          deleteRole();
+          break;
+        case "view a deparment budget":
+          viewBudget();
+        default:
+          console.log("you are exiting");
+          break;
+      }
+    });
+}
 
 
-        }
-        
