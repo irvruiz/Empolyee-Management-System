08b08f6a23e768046bd6902524070a6d7a05e5fc
Irvin <irvinruiz44@yahoo.com>
2022-02-17 19:35:57 -0700

    added js

--- index.js
+++ index.js
@@ -198,3 +198,157 @@ db.query(`SELECT * FROM department;`
 
         })
     })
+
+}
+function addEmployee() {
+    db.query(`Select * From company_db.employee ;`, function (err, results) {
+        return inquirer.prompt([
+            {
+                type: "input",
+                name: "employeeFName",
+                message: "Please enter the employees first name"
+            },
+            {
+                type: "input",
+                name: "employeeLName",
+                message: "Please enter the employees last name"
+            },
+            {
+                type: "input",
+                name: "employeeRole",
+                message: "Please enter a role for the employee"
+            }
+        ]).then((results) => {
+            var empLN = results.employeeLName;
+            var empFN = results.employeeFName;
+            var empRoleID = results.employeeRole;
+            console.log("sid")
+            console.table(results);
+            db.query(`select * from company_db.employee where manager_id is null;`, function (err, results) {
+                var employeeArr = [];
+                results.forEach(result => employeeArr.push({ name: result.first_name + ' ' + result.last_name, value: result.id }));
+                return inquirer.prompt([
+                    {
+                        type: "list",
+                        name: "employeeManager",
+                        message: "Who is the employee's manager?",
+                        choices: employeeArr
+                    },
+                ]).then((data) => {
+                    db.query(`INSERT  INTO company_db.employee (company_db.employee.first_name, company_db.employee.last_name, company_db.employee.role_id, company_db.employee.manager_id) Values("${empFN}", "${empLN}",${empRoleID}, ${data.employeeManager})`, function (err, results) { // working, the placeholder needed to be in ()
+                        if (err) throw new Error("query failure : ", err);
+
+                    })
+                    viewEmployees();
+                    promptUser()
+
+                })
+            })
+        })
+    })
+}
+//Update
+employeeArr = [];
+function updateEmployeeRole() {
+    db.query(`Select * From company_db.employee;`, function (err, results) {
+
+        results.forEach(result => employeeArr.push({ name: result.first_name + ' ' + result.last_name, value: result.id }));
+        return inquirer.prompt([
+            {
+                type: "list",
+                name: "employeeList",
+                choices: employeeArr,
+                message: "Please select an employee to update"
+            },
+            {
+                type: "input",
+                name: "updateVal",
+                message: "Please enter a role number"
+            }
+        ]).then((data) => {
+            var { employeeList, updateVal } = data
+            var [value] = employeeArr;
+            console.log("siddy", employeeList)
+            console.log("siddy2", updateVal)
+            console.log("siddy3", value.value)
+            db.query(`UPDATE company_db.employee set role_id = ${Number(updateVal)} where id = ${employeeList} ;`, function (err, results ) {
+                if (err) throw new Error("query failure : " , err);
+
+            })
+            displayEmployeeTable();
+
+        })
+
+    })
+
+}
+//Delete
+function deleteDepartment() {
+    db.query(`SELECT * FROM company_db.department;`, function (err, results) {
+        let departArray = [];
+        results.forEach(result => departArray.push({ name: result.name, value: result.id }));
+        return inquirer.prompt([
+            {
+                type: "list",
+                name: "deleteDepartment",
+                message: "Please choice a department to delete?",
+                choices: departArray
+            },
+        ]).then((data) => {
+            db.query(`DELETE FROM department WHERE id = ${data.deleteDepartment};`, function (err, results) {
+                promptUser()
+            })
+
+        })
+    })
+};
+function deleteRole() {
+    db.query(`SELECT * FROM company_db.role;`, function (err, results) {
+        let rolArray = [];
+
+        results.forEach(result => rolArray.push({ name: result.title , value: result.id }));
+        
+        return inquirer.prompt([
+            {
+                type: "list",
+                name: "deleteRole",
+                message: "Please choice a role to delete?",
+                choices: rolArray
+            },
+        ]).then((data) => {
+
+            db.query(`DELETE FROM company_db.role WHERE id = ${data.deleteRole};`, function (err, results) {
+                promptUser()
+            })
+
+           
+        })
+          
+    })
+}
+function deleteEmployee() {
+
+    db.query(`SELECT * FROM company_db.employee;`, function (err, results) {
+        let empArray = [];
+
+        results.forEach(result => empArray.push({ name: result.first_name + ' ' + result.last_name, value: result.id }));
+        
+        return inquirer.prompt([
+            {
+                type: "list",
+                name: "deleteEmployee",
+                message: "Please choice a employee to delete?",
+                choices: empArray
+            },
+        ]).then((data) => {
+         
+            db.query(`DELETE FROM company_db.employee WHERE id = ${data.deleteEmployee};`, function (err, results) {
+                promptUser()
+            })
+
+           
+        })
+          
+    })
+
+}
