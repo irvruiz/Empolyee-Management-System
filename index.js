03d6b947bf8590c383555f60b23576dcdf568a7c
Irvin <irvinruiz44@yahoo.com>
2022-02-17 18:02:42 -0700

    added js

--- index.js
+++ index.js
@@ -12,3 +12,22 @@ const db = mysql.createConnection(
     },
     console.log("Connected to the company_db database....")
 );
+
+db.connect((err) => {
+    if (err) {
+        throw error;
+    }
+});
+//inqurirer
+promptUser()
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "displayChoices",
+            message: "Please choose an option",
+            choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role", "delete an employee", "delete a department",  "delete a role", "view a deparment budget"]
+
+
+        }
+        
